a220aa7b1ddc37a6d650ccd2d3a53e49ec233547
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2024-05-29 08:03:00 -0500

    Implementacion final.

--- tcp/ejemplo_01/src/Client.java
+++ tcp/ejemplo_01/src/Client.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public Client(String ip, int puerto) {
@@ -18,9 +19,21 @@ public class Client {
         OutputStreamWriter out_writer = new OutputStreamWriter(socket.getOutputStream());
         PrintWriter out_socket = new PrintWriter(out_writer, true);
 
-        
+        String mensaje = in_socket.readLine();
+        System.out.println( "Servidor dice: " + mensaje );
+
+        out_socket.println("Gracias.");
+
+        socket.close();
+        System.out.println("Socket cerrado.");
     }
     public static void main(String[] args) {
+        Scanner consola = new Scanner();
+        System.out.print("Ingresar IP: ");
+        String ip = consola.nextLine();
+        System.out.print("Ingresar puerto: ");
+        Integer puerto = Integer.parseInt( consola.nextLine() );
         
+        new Client(ip, puerto);
     }
 }
