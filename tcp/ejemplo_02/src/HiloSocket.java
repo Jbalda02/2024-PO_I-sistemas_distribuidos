60143fad07c3e91b37f5d1835657b484312a43bb
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2024-06-05 07:56:57 -0500

    Implementacion del Servidor.

--- tcp/ejemplo_02/src/HiloSocket.java
+++ tcp/ejemplo_02/src/HiloSocket.java
@@ -1,5 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class HiloSocket extends Thread {
     
@@ -10,7 +16,38 @@ public class HiloSocket extends Thread {
     }
 
     public void run() {
-        Servicio servicio = new Servicio();
-        servicio.inicio(this.socket);
+        try {
+            // I/O Buffers
+            // Entrada
+            InputStreamReader in_reader = new InputStreamReader( this.socket.getInputStream() );
+            BufferedReader in_socket = new BufferedReader(in_reader);
+
+            // Salida
+            OutputStreamWriter out_writer = new OutputStreamWriter(this.socket.getOutputStream());
+            PrintWriter out_socket = new PrintWriter(out_writer, true);
+
+            String input = "", output = "";
+
+            while((input = in_socket.readLine()) != null) {
+                System.out.println( input );
+
+                if (input.equals("hola")) {
+                    output = "Saludos.";
+                } else if (input.equals("hora")) {
+                    output = (new Date()).toString();
+                } else if (input.equals("adios")) {
+                    output = "Que te vaya bien.";
+                    out_socket.println(output);
+                    out_socket.flush();
+                    break;
+                }
+
+                out_socket.println(output);
+                out_socket.flush();
+            }
+
+            this.socket.close();
+        } catch(IOException ex) {
+        } 
     }
 }
